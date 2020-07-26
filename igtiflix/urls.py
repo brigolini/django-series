01c0fd91b2fdcc621746c44307b6522398ffaf67
Marcelo Sampaio Brigolini Silva <sampaiomarcelo@gmail.com>
2020-07-26 14:06:21 -0300

    02 - Create First Template

--- igtiflix/urls.py
+++ igtiflix/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from principal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/',views.index)
+    path(route='principal/',view=include('principal.urls'))
 ]
--- principal/views.py
+++ principal/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+   return render(request,'index.html')
