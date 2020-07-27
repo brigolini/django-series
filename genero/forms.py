620ee85b33950bed0e1a011cfe9069283af07c3d
Marcelo Sampaio Brigolini Silva <sampaiomarcelo@gmail.com>
2020-07-26 21:19:47 -0300

    06 - Save data into the database and show it

--- genero/forms.py
+++ genero/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from genero.models import Genero
 
-class GeneroForm(forms.Form):
+class GeneroForm(forms.ModelForm):
 
-    def __init__(self, *args, **kwargs):
-        super(GeneroForm, self).__init__(*args, **kwargs)
-        self.fields['genero'].error_messages = {'required': 'Campo obrigatório'}
 
-    descricao = forms.CharField(label='Gênero:',required=True)
+
+    class Meta:
+        model = Genero
+        fields = '__all__'
--- genero/views.py
+++ genero/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+
 from . import forms
+from . import models
 # Create your views here.
 
 def cadastro(request):
-   form = forms.GeneroForm(request.POST)
 
+   form = forms.GeneroForm(request.POST)
    if request.method == 'POST':
       if form.is_valid():
-         print("Validado com sucesso")
-         print(form.cleaned_data['genero'])
+         form.save(commit=True)
+      else:
+         print("ERROR")
+#   if request.method == 'POST':
+#      if form.is_valid():
+#         print("Validado com sucesso")
+#         print(form.cleaned_data['descricao'])
+   generos_list = models.Genero.objects.order_by('descricao')
+   date_dict = {"genero_records": generos_list,'form':form}
 
-   return render(request,'genero/genero.html',{'form':form})
+   return render(request,'genero/genero.html',date_dict)
