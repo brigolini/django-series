1bc544e2ddb31efd704bf47fb9aae6cdb184e0a5
Marcelo Sampaio Brigolini <51216384+brigolini@users.noreply.github.com>
2020-07-28 19:06:15 -0300

    Implement update

--- genero/urls.py
+++ genero/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.cadastro,name='cadastro'),
-    path('delete/<id>',views.delete,name='cadastro'),
+    path('delete/<id>',views.delete,name='delete'),
+    path('update/<id>',views.update,name='update'),
 ]
--- genero/views.py
+++ genero/views.py
@@ -5,20 +5,41 @@ from . import models
 
 
 def cadastro(request):
-   if (request.method=='DELETE'):
-      print("DELETIZOU")
-   form = forms.GeneroForm()
-   if request.method == 'POST':
-      form = forms.GeneroForm(request.POST)
-      if form.is_valid():
-         form.save(commit=True)
-      else:
-         print("ERROR")
-   generos_list = models.Genero.objects.order_by('descricao')
-   date_dict = {"genero_records": generos_list,'form':form}
+    print("insert")
+    print(request.method);
+    form = forms.GeneroForm()
+    if request.method == 'POST':
+        form = forms.GeneroForm(request.POST)
+        if form.is_valid():
+            print("Saving")
+            form.save(commit=True)
+        else:
+            print("ERROR")
+    generos_list = models.Genero.objects.order_by('descricao')
+    data_dict = {"genero_records": generos_list, 'form': form}
 
-   return render(request,'genero/genero.html',date_dict)
+    return render(request, 'genero/genero.html', data_dict)
 
-def delete(request,id):
-   models.Genero.objects.filter(id=id).delete()
-   return render(request,'genero/genero.html')
+
+def delete(request, id):
+    print("delete")
+    models.Genero.objects.filter(id=id).delete()
+    form = forms.GeneroForm()
+    generos_list = models.Genero.objects.order_by('descricao')
+    data_dict = {"genero_records": generos_list, 'form': form}
+    return render(request, 'genero/genero.html', data_dict)
+
+
+def update(request, id):
+    item = models.Genero.objects.get(id=id);
+    if request.method == "GET":
+        form = forms.GeneroForm(initial={'descricao': item.descricao})
+        data_dict = {'form': form}
+        return render(request, 'genero/genero_upd.html', data_dict)
+    else:
+        form = forms.GeneroForm(request.POST)
+        item.descricao = form.data['descricao']
+        item.save()
+        generos_list = models.Genero.objects.order_by('descricao')
+        data_dict = {"genero_records": generos_list, 'form': form}
+        return render(request, 'genero/genero.html', data_dict)
